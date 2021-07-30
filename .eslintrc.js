63035e0cfece668297651e3a2ed3672886358057
shi <jsclndnz@gmail.com>
2021-07-30 10:40:47 +0800

    crlf

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,14 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
   parserOptions: {
     parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'linebreak-style': ['off', 'windows'],
     'no-unused-vars': 'off',
     'vue/no-unused-components': 'off',
   },
