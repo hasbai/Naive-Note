42369c98f8bf341a6154a06143176f9afb74922e
shi <jsclndnz@gmail.com>
2021-07-30 10:29:50 +0800

    eslint 使用 prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
   parserOptions: {
     parser: 'babel-eslint',
   },
