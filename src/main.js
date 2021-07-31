b863d2c9918e18abfba30ddc3b43b881b0801cff
shi <jsclndnz@gmail.com>
2021-07-31 16:16:37 +0800

    移动vuex配置至@/vuex/index.js

--- src/main.js
+++ src/main.js
@@ -1,71 +1,12 @@
 import { createApp } from 'vue'
-import { createStore } from 'vuex'
-import createPersistedState from 'vuex-persistedstate'
+
 import App from './App.vue'
-import naive from 'naive-ui'
 import router from './router'
-import { createClient } from 'webdav'
-// 创建一个新的 store 实例
-const store = createStore({
-  plugins: [
-    createPersistedState({
-      paths: ['webdavConfig', 'folders'],
-    }),
-  ],
-  state() {
-    return {
-      webdavConfig: {},
-      folders: [],
-      showWebdavConfig: false,
-      showFolderSelector: false,
-    }
-  },
-  getters: {
-    client(state) {
-      const webdavConfig = state.webdavConfig
-      return createClient(webdavConfig.url, {
-        username: webdavConfig.username,
-        password: webdavConfig.password,
-      })
-    },
-  },
-  mutations: {
-    setWebdavConfig(state, config) {
-      state.webdavConfig = config
-    },
-    displayWebdavConfig(state) {
-      state.showWebdavConfig = true
-    },
-    closeWebdavConfig(state) {
-      state.showWebdavConfig = false
-    },
-    displayFolderSelector(state) {
-      state.showFolderSelector = true
-    },
-    closeFolderSelector(state) {
-      state.showFolderSelector = false
-    },
-    addFolders(state, folders) {
-      state.folders.push.apply(state.folders, folders)
-    },
-    deleteFolders(state, nodeKeys) {
-      nodeKeys.forEach((key) => {
-        const index = state.folders.findIndex((node) => {
-          return node.key === key
-        })
-        if (index >= 0) {
-          state.folders.splice(index, 1)
-        }
-      })
-    },
-    setFolders(state, folders) {
-      state.folders = folders
-    },
-  },
-})
+import store from './vuex'
+import naive from 'naive-ui'
 
 const app = createApp(App)
 app.use(router)
-app.use(naive)
 app.use(store)
+app.use(naive)
 app.mount('#app')
---
+++ src/vuex/index.js
@@ -0,0 +1,87 @@
+import { createStore } from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+import { createClient } from 'webdav'
+
+const store = createStore({
+  plugins: [
+    createPersistedState({
+      paths: ['webdavConfig', 'folders'],
+    }),
+  ],
+  state() {
+    return {
+      webdavConfig: {},
+      folders: [],
+      unsavedFiles: [],
+      showWebdavConfig: false,
+      showFolderSelector: false,
+    }
+  },
+  getters: {
+    client(state) {
+      const webdavConfig = state.webdavConfig
+      return createClient(webdavConfig.url, {
+        username: webdavConfig.username,
+        password: webdavConfig.password,
+      })
+    },
+    // folders(state) {
+    //   return state.folders.map(node => {
+    //     return {
+    //       label: node.label,
+    //       key: node.key,
+    //       prefix
+    //     }
+    //   })
+    // }
+  },
+  mutations: {
+    setWebdavConfig(state, config) {
+      state.webdavConfig = config
+    },
+    displayWebdavConfig(state) {
+      state.showWebdavConfig = true
+    },
+    closeWebdavConfig(state) {
+      state.showWebdavConfig = false
+    },
+    displayFolderSelector(state) {
+      state.showFolderSelector = true
+    },
+    closeFolderSelector(state) {
+      state.showFolderSelector = false
+    },
+    addFolders(state, folders) {
+      state.folders.push.apply(state.folders, folders)
+    },
+    deleteFolders(state, nodeKeys) {
+      nodeKeys.forEach((key) => {
+        const index = state.folders.findIndex((node) => {
+          return node.key === key
+        })
+        if (index >= 0) {
+          state.folders.splice(index, 1)
+        }
+      })
+    },
+    setFolders(state, folders) {
+      state.folders = folders
+    },
+    modifyUnsavedFiles(state, payload) {
+      const key = payload.key
+      const method = payload.method
+      const index = state.unsavedFiles.indexOf(key)
+      if (method === 'add') {
+        if (index === -1) {
+          state.unsavedFiles.push(key)
+        }
+      } else if (method === 'delete') {
+        if (index !== -1) {
+          state.unsavedFiles.splice(index, 1)
+        }
+      }
+    },
+  },
+})
+
+export default store
