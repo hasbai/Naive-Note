0e0a5d09f0931d7d89b9afa4d1f59174a2012453
shi <jsclndnz@gmail.com>
2021-07-31 20:31:04 +0800

    修复编辑器字数统计样式的bug

--- src/App.vue
+++ src/App.vue
@@ -23,18 +23,23 @@
   </div>
 </template>
 
+<!--suppress CssUnusedSymbol -->
 <style>
 .footer {
   height: 64px;
   padding: 24px;
 }
 
-/*noinspection CssUnusedSymbol*/
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+/*vditor的字数统计，鼠标悬停时会变成气泡，与上方部件冲突，设置position: static属性可避免*/
+.vditor-toolbar .vditor-tooltipped {
+  position: static;
 }
 </style>
