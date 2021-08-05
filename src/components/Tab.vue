2b6fb645ac743345b997067776c664902da223cc
shi <jsclndnz@gmail.com>
2021-08-05 17:01:35 +0800

    标签页动态改变组件

--- src/components/Tab.vue
+++ src/components/Tab.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="tab-container">
     <n-tabs
-      v-model:value="localViewingKey"
+      :value="viewingKey"
       type="card"
       closable
       @close="close"
+      @update:value="onUpdate"
       tab-style="min-width: 80px"
     >
       <n-tab-pane
@@ -25,13 +26,6 @@
         ref="tab"
       ></component>
     </keep-alive>
-
-    <!--      <Editor-->
-    <!--        v-if="viewingKey"-->
-    <!--        :key="viewingKey"-->
-    <!--        :path="viewingKey"-->
-    <!--        ref="editor"-->
-    <!--      ></Editor>-->
   </div>
 </template>
 
@@ -40,15 +34,14 @@ import { shallowRef } from 'vue'
 import { useMessage } from 'naive-ui'
 import Editor from '@/components/Editor.vue'
 import PDFViewer from '@/components/PDFViewer.vue'
+import Welcome from '@/views/Welcome.vue'
 export default {
   name: 'Tab',
-  components: { Editor },
+  components: {},
   data() {
     return {
       cachedEditors: [], // 所有已渲染的编辑器
       componentName: '',
-      componentProps: {},
-      localViewingKey: '',
     }
   },
   computed: {
@@ -65,22 +58,37 @@ export default {
   },
   watch: {
     viewingKey() {
-      this.localViewingKey = this.viewingKey
-    },
-    localViewingKey() {
-      this.$store.commit('setViewingKey', this.localViewingKey)
+      if (this.viewingKey) {
+        this.changeComponent(this.viewingKey)
+      }
     },
   },
   methods: {
-    findIndex(key) {
-      // 对象数组不能用 includes 判断
-      return this.tabs.findIndex((file) => key === file.key)
+    changeComponent(key) {
+      // 当 viewingKey 改变时动态改变组件
+      const node = this.tabs.find((node) => key === node.key)
+      if (node.mime === 'text/markdown') {
+        this.componentName = shallowRef(Editor)
+      } else if (node.mime === 'application/pdf') {
+        this.componentName = shallowRef(PDFViewer)
+      } else if (node.mime.startsWith('text')) {
+        this.message.warning('文本文件不是 markdown 格式')
+        this.componentName = shallowRef(Editor)
+      } else {
+        // 切换至默认页面
+        this.message.error('文件格式不支持')
+        this.componentName = shallowRef(Welcome)
+      }
+    },
+    onUpdate(key) {
+      // 选中项改变时的回调函数
+      this.$store.commit('setViewingKey', key)
     },
     close(key) {
       this.$store.commit('closeTab', key)
     },
     add(node) {
-      const isOpened = this.findIndex(node.key) >= 0
+      const isOpened = this.tabs.findIndex((file) => node.key === file.key) >= 0
       const isCached = this.cachedEditors.includes(node.key)
       // 打开标签页
       if (!isOpened) {
@@ -91,13 +99,6 @@ export default {
         this.cachedEditors.push(node.key)
       }
       // 切换至（如没有则创建）编辑器
-      if (node.mime === 'text/markdown') {
-        this.componentName = shallowRef(Editor)
-      } else if (node.mime === 'application/pdf') {
-        this.componentName = shallowRef(PDFViewer)
-      } else {
-        return
-      }
       this.$store.commit('setViewingKey', node.key)
       // 如果 markdown 编辑器已缓存且标签页已打开，再次点击则更新内容
       if (isOpened && isCached && node.mime === 'text/markdown') {
