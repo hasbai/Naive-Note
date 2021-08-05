b56e98bff07704e849fe494c4b8671ffd388fdca
shi <jsclndnz@gmail.com>
2021-08-05 17:01:17 +0800

    默认欢迎页面

---
+++ src/views/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="margin-top: 200px">
+    <n-result status="success" title="欢迎" description="">
+      <template #footer>
+        <n-button>Naive Note</n-button>
+      </template>
+    </n-result>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+}
+</script>
+
+<style scoped></style>
