fd0b7900e5683a02ddce8a29a65fa20f8951dffb
shi <jsclndnz@gmail.com>
2021-08-06 15:48:45 +0800

    上传文件功能

--- src/components/CreateSomething.vue
+++ src/components/CreateSomething.vue
@@ -7,7 +7,17 @@
     :title="title"
     :bordered="false"
   >
+    <n-upload
+      v-if="showUpload"
+      multiple
+      @change="handleChange"
+      ref="upload"
+      :class="showInput ? 'upload' : ''"
+    >
+      <n-button>选择文件</n-button>
+    </n-upload>
     <n-input
+      v-if="showInput"
       v-model:value="value"
       type="text"
       placeholder="叫什么？"
@@ -39,6 +49,7 @@ export default {
   data() {
     return {
       value: '',
+      fileList: [],
       buttonLoading: false,
       message: useMessage(),
     }
@@ -50,32 +61,34 @@ export default {
     title() {
       return this.mode === 'file' ? '创建文件' : '创建文件夹'
     },
+    showUpload() {
+      return this.mode === 'file' && !this.value
+    },
+    showInput() {
+      return this.fileList.length === 0
+    },
   },
-  watch: {
-    mode() {},
-  },
+  watch: {},
   methods: {
     async submit() {
-      // 校验文件名非空
-      this.value = this.value.trim()
-      if (this.value === '') {
-        this.message.error('内容不能为空')
-        return false
-      }
-      // 构造路径
-      const parentPath =
-        this.createFileParent.key === '/' ? '' : this.createFileParent.key // 根目录为 '/'，其它目录末尾无 '/'
-      const path = parentPath + '/' + this.value
-      // 发送请求
-      this.buttonLoading = true
       try {
-        if (this.mode === 'file') {
-          // 创建文件
-          await this.client.putFileContents(path, '', {
-            contentLength: false,
-          })
-        } else {
-          await this.client.createDirectory(path)
+        if (this.showInput) {
+          await this.upload(this.value)
+        } else if (this.showUpload) {
+          while (this.fileList.length > 0) {
+            let item = this.fileList[this.fileList.length - 1]
+            item.status = 'uploading'
+            let timer = setInterval(() => {
+              if (item.status !== 'uploading') {
+                clearInterval(timer)
+              } else {
+                item.percentage += 1
+              }
+            }, 20)
+            await this.upload(item.name, item.file)
+            item.status = 'finished'
+            this.fileList.pop()
+          }
         }
         this.message.success(this.title + '成功')
         this.$emit('success')
@@ -87,6 +100,33 @@ export default {
         this.buttonLoading = false
       }
     },
+    async upload(name, data = '') {
+      // 校验文件名非空
+      name = name.trim()
+      if (name === '') {
+        this.message.error('内容不能为空')
+        return false
+      }
+      // 构造路径
+      const parentPath =
+        this.createFileParent.key === '/' ? '' : this.createFileParent.key // 根目录为 '/'，其它目录末尾无 '/'
+      const path = parentPath + '/' + name
+      // 发送请求
+      this.buttonLoading = true
+
+      if (this.mode === 'file') {
+        // 创建文件
+        await this.client.putFileContents(path, data, {
+          contentLength: false,
+        })
+      } else {
+        // 创建文件夹
+        await this.client.createDirectory(path)
+      }
+    },
+    handleChange({ fileList }) {
+      this.fileList = fileList
+    },
     close() {
       this.$emit('close')
     },
@@ -94,4 +134,9 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+/*noinspection ALL*/
+.upload {
+  padding-bottom: 1rem;
+}
+</style>
